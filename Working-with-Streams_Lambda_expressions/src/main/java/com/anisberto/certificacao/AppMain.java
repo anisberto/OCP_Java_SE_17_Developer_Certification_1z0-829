fcf238eb3ac37b1ddd68b1e38e140d22c752492e
anisberto dos reis do nascimento <anisbertoos@gmail.com>
2024-05-30 21:53:29 -0300

    reestruturação dos fontes

--- Working-with-Streams_Lambda_expressions/src/main/java/com/anisberto/certificacao/AppMain.java
+++ Working-with-Streams_Lambda_expressions/src/main/java/com/anisberto/certificacao/AppMain.java
@@ -1,7 +1,15 @@
 package com.anisberto.certificacao;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class AppMain {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) {
+		List<Integer> lista = Arrays.asList(1, 2, 3, 4);
+		
+		Stream<Integer> mapa = lista.stream().limit(3).map(e -> e * 2);
+		
+		mapa.forEach(e -> System.out.println(e));
+	}
 }
