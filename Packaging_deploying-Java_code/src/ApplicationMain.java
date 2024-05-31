9fe7be559ea1803eace2d9473f93a57114a54d4b
anisberto dos reis do nascimento <anisbertoos@gmail.com>
2024-05-30 21:45:47 -0300

    reestruturação dos fontes

--- Packaging_deploying-Java_code/src/ApplicationMain.java
+++
@@ -1,9 +0,0 @@
-import java.util.logging.Logger;
-
-public class ApplicationMain {
-	private static final Logger logger = Logger.getLogger(ApplicationMain.class.getName());
-
-	public static void main(String[] args) {
-		logger.info("Hello, Java 17!");
-	}
-}
---
+++ Working-with-Streams_Lambda_expressions/src/main/java/com/anisberto/certificacao/AppMain.java
@@ -0,0 +1,7 @@
+package com.anisberto.certificacao;
+
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
