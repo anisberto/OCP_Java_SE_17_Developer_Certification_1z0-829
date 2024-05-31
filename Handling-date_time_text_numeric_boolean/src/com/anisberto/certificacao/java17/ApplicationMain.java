7121915ad36bdb6f9a8e6da5d5d349f006e477bf
anisberto dos reis do nascimento <anisbertoos@gmail.com>
2024-05-30 23:56:47 -0300

    criação de estrutura em codigo

--- Handling-date_time_text_numeric_boolean/src/com/anisberto/certificacao/java17/ApplicationMain.java
+++ Handling-date_time_text_numeric_boolean/src/com/anisberto/certificacao/java17/ApplicationMain.java
@@ -1,7 +1,9 @@
-package com.anisberto.certificacao.java17;
+import java.util.logging.Logger;
 
 public class ApplicationMain {
+	private static final Logger logger = Logger.getLogger(ApplicationMain.class.getName());
+
 	public static void main(String[] args) {
-		
+		logger.info("Hello, Java 17!");
 	}
 }
