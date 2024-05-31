0de10983dd8b433b10c9bfa6753d9f57cd1bfc82
anisberto dos reis do nascimento <anisbertoos@gmail.com>
2024-05-31 00:00:04 -0300

    criação de estrutura em codigo

---
+++ Controlling_Program_Flow/src/ApplicationMain.java
@@ -0,0 +1,9 @@
+import java.util.logging.Logger;
+
+public class ApplicationMain {
+	private static final Logger logger = Logger.getLogger(ApplicationMain.class.getName());
+
+	public static void main(String[] args) {
+		logger.info("Hello, Java 17!");
+	}
+}
--- Managing-concurrent_code_execution/src/ApplicationMain.java
+++ Managing-concurrent_code_execution/src/ApplicationMain.java
@@ -1,4 +1,4 @@
-package com.anisberto.certificacao.java17;
+
 
 public class ApplicationMain {
 	public static void main(String[] args) {
